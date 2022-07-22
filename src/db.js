5ee9e275e8910c5b480ba9c1c69d6346a1549242
adrian411997 <adrian.mart41197@gmail.com>
2022-07-22 14:00:28 -0300

    please

--- src/db.js
+++ src/db.js
@@ -27,7 +27,7 @@ fs.readdirSync(path.join(__dirname, "/Models"))
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+    modelDefiners.push(require(path.join(__dirname, "/Models", file)));
   });
 
 modelDefiners.forEach((model) => model(sequelize));
